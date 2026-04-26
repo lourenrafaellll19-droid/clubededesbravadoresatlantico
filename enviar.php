1066298f318ca736a104c17a06c667ac7b7fe572
clubededesbravadoresatlantico <lourenrafaellll19@gmail.com>
2026-04-26 00:55:18 -0300

    Update enviar.php

--- enviar.php
+++ enviar.php
@@ -7,7 +7,7 @@ $telefone = $_POST['telefone'];
 
 // Pasta onde a foto será salva
 $diretorio = "uploads/";
-if (!is_dir($diretorio)) {
+hif (!is_dir($diretorio)) {
     mkdir($diretorio, 0755, true);
 }
 
